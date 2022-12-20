6fcf7b58b351a50e53cae3cca6118b124f07577e
Ruslan Mansurov <ruslanways@gmail.com>
2022-12-20 17:45:49 +0100

    make some beaty to codestyle task_1.py

--- task_1.py
+++ task_1.py
@@ -13,45 +13,72 @@ Use supplied system account credentials when authenticating with GCP APIs in
 both cases.
 """
 
-user = "po5sj576o13t3n9m72fhc2d5r0fzd1l4c9w2kggkd11lkisqlvfan8016n1g1dvk"
-pwd = "pgpn186dbc7gpzl8gj1g0aiflu5cj6hu7xy0lnejfc4invuwu92tt3316w5kupgo"
-
-# Get a base64 encoded string needs as credential token to Bloomreach Engagement.
-credentials = f"{user}:{pwd}"
-credentials_bytes = credentials.encode()
-credentials_base64 = base64.b64encode(credentials_bytes)
-credentials_base64_str = credentials_base64.decode()
-
-url = "https://api.exponea.com/data/v2/projects/962d1b9a-481b-11ea-9329-6a9665f4e6e6/customers/export"
-payload = {"format": "table_json"}
-headers = {
-    "accept": "application/json",
-    "authorization": f"Basic {credentials_base64_str}",
-}
-response = requests.post(url, json=payload, headers=headers)
-response_dict = response.json()  # Get python dict from response json.
-
-# Create pandas DataFrame from the dict.
-df = pd.DataFrame(data=response_dict["rows"], columns=response_dict["header"])
-
-# Cast all columns of DataFrame to string data types for exporting to BigQuery.
-df = df.astype(str)
-
-# Get authentication credentials for BigQuery connection.
-credentials_gcp = service_account.Credentials.from_service_account_file(
-    Path(__file__).resolve().parent/"credentials_gcp.json",
-)
-
-# Send data from DataFrame to BigQuery table by chunks of 20 lines.
-df.to_gbq(
-    "Ruslan_Mansurov_coding_exercise.bloomreach_task",
-    "wisdom-dev-340814",
-    if_exists="replace",
-    chunksize=20,
-    credentials=credentials_gcp,
-)
+USER = "po5sj576o13t3n9m72fhc2d5r0fzd1l4c9w2kggkd11lkisqlvfan8016n1g1dvk"
+PWD = "pgpn186dbc7gpzl8gj1g0aiflu5cj6hu7xy0lnejfc4invuwu92tt3316w5kupgo"
+CREDENTIALS_GCP_JSON_FILE = "credentials_gcp.json"
+PROJECT_ID = "wisdom-dev-340814"
+DATASET_ID = "Ruslan_Mansurov_coding_exercise"
+TABLE_ID = "bloomreach_task"
 
 
+def main(user, pwd, credentials, project, dataset, table):
+    """Get data from Bloomreach Engagement api
+    and send it to BigQuery.
+    """
+    df = read_from_bloomreach(user, pwd)
+    send_to_bogquery(df, credentials, project, dataset, table)
+
+
+def read_from_bloomreach(user, pwd):
+    """Make a HTTP-request to get the data
+    and transform it to Pandas.DataFrame.
+    """
+    # Get a base64 encoded string needs as credential token to Bloomreach Engagement.
+    credentials = f"{user}:{pwd}"
+    credentials_bytes = credentials.encode()
+    credentials_base64 = base64.b64encode(credentials_bytes)
+    credentials_base64_str = credentials_base64.decode()
+
+    url = "https://api.exponea.com/data/v2/projects/962d1b9a-481b-11ea-9329-6a9665f4e6e6/customers/export"
+    payload = {"format": "table_json"}
+    headers = {
+        "accept": "application/json",
+        "authorization": f"Basic {credentials_base64_str}",
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    response_dict = response.json()  # Get python dict from response json.
+
+    # Create pandas DataFrame from the dict.
+    return pd.DataFrame(data=response_dict["rows"], columns=response_dict["header"])
+
+
+def send_to_bogquery(df, credentials, project, dataset, table):
+    """Connect to BigQuery
+    and send the data to it.
+    """
+
+    # Cast all columns of DataFrame to string data types for exporting to BigQuery.
+    df = df.astype(str)
+
+    # Get authentication credentials for BigQuery connection.
+    credentials_gcp = service_account.Credentials.from_service_account_file(
+        Path(__file__).resolve().parent / credentials,
+    )
+
+    # Send data from DataFrame to BigQuery table by chunks of 20 lines.
+    df.to_gbq(
+        f"{dataset}.{table}",
+        f"{project}",
+        if_exists="replace",
+        chunksize=20,
+        credentials=credentials_gcp,
+    )
+
+
+if __name__ == "__main__":
+    main(USER, PWD, CREDENTIALS_GCP_JSON_FILE, PROJECT_ID, DATASET_ID, TABLE_ID)
+
+# # # # #
 # Sending data to BigQuery in chunks method 2.
 #
 # step = 20
